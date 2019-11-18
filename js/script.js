a4fbf2a008398d64c037f01e90ee96ba7e092ec6
Koallla <zmiyov2422@gmail.com>
2019-11-18 20:28:13 +0200

    weather API

--- js/script.js
+++ js/script.js
@@ -76,18 +76,104 @@
 
 // =======================================================
 
-// const listItem = '<li class="list-item"></li>';
-const list = document.querySelector(".list");
+// const list = document.querySelector(".list");
 
-fetch("https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11")
-  .then(response => {
-    return response.json();
-  })
+// fetch("https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5")
+//   .then(response => {
+//     return response.json();
+//   })
+//   .then(data => {
+//     const markup = data
+
+//       .map(
+//         value =>
+//           `<li class="list-item">Buy ${value.ccy} ${value.buy} Sale ${value.sale}</li>`
+//       )
+//       .join(" ");
+
+//     list.insertAdjacentHTML("afterbegin", markup);
+//   });
+
+// =======================================================
+
+// const postToAdd = {
+//   author: "Mango",
+//   body: "CRUD is awesome"
+// };
+
+// fetch("https://jsonplaceholder.typicode.com/posts", {
+//   method: "POST",
+//   body: JSON.stringify(postToAdd),
+//   headers: {
+//     "Content-Type": "application/json; charset=UTF-8"
+//   }
+// })
+//   .then(response => response.json())
+//   .then(post => console.log(post))
+//   .catch(error => console.log(error));
+
+// =======================================================
+// FETCH
+// fetch("https://swapi.co/api/planets")
+//   .then(res => res.json())
+//   .then(data => {
+//     const planets = data.results;
+//     let i = 0;
+
+//     const planetName = planets.map(planet =>
+//       console.log(`${i++} ` + planet.name)
+//     );
+//   });
+
+// https://api.darksky.net/forecast/[key]/[latitude],[longitude]
+// key DarkSky = 18648bab4c8328661bc43b01df85e327;
+// buypassCORS = https://cors-anywhere.herokuapp.com/
+// api.openweathermap.org/data/2.5/weather?q=London
+
+//   AIzaSyBD04XP72ay8ijDkORFep0J-yUMtQOjVdo - мой ключ Google
+
+// =======================================================
+// OPENWEATHER
+// const key = "48d7ba55d76cf45b84f2e89c5d99ee83";
+// //api.openweathermap.org/data/2.5/weather?q=London,uk&appid=YOUR_API_KEY
+
+// fetch("https://api.darksky.net/forecast/[key]/[latitude],[longitude]")
+//   .then(res => res.json())
+//   .then(data => console.log(data));
+
+// =======================================================
+// APIXU
+// key = e8278975abb258561367cab2590794a3;
+
+// const baseUrl = "http://api.weatherstack.com/";
+
+fetch(
+  "http://api.weatherstack.com/current?access_key=e8278975abb258561367cab2590794a3&query= fetch:ip"
+)
+  .then(res => res.json())
   .then(data => {
-    const markup = data
+    const temperature = data.current.temperature;
+    const country = data.location.country;
+    const localtime = data.location.localtime;
+    const region = data.location.region;
+    const name = data.location.name;
+    const windSpeed = data.current.wind_speed;
+    const cloudcover = data.current.cloudcover;
+    const weather_icons = data.current.weather_icons[0];
 
-      .map(value => `<li class="list-item">Buy ${value.ccy} ${value.buy}</li>`)
-      .join(" ");
+    const markup = `
+      <li class="list-item">Temperature: ${temperature} C</li>
+      <li class="list-item">Wind speed: ${windSpeed} m/s</li>
+      <li class="list-item">Cloudcover: ${cloudcover}</li>
+      <li class="list-item">Country: ${country}</li>
+      <li class="list-item">Region: ${region}</li>
+      <li class="list-item">Name: ${name}</li>
+      <li class="list-item">Localtime: ${localtime}</li>`;
 
-    list.insertAdjacentHTML("afterbegin", markup);
+    const image = document.createElement("img");
+    image.setAttribute("src", weather_icons);
+
+    const list = document.querySelector(".list");
+
+    list.innerHTML = markup;
   });
