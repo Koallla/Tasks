bda7b0479d5c25b062dddbb789a2d9571b3973da
Koallla <zmiyov2422@gmail.com>
2019-11-18 21:00:43 +0200

    Weather API New

--- Training/js/script.js
+++ Training/js/script.js
@@ -146,7 +146,7 @@
 // const baseUrl = "http://api.weatherstack.com/";
 
 fetch(
-  "http://api.weatherstack.com/current?access_key=e8278975abb258561367cab2590794a3&query= fetch:ip"
+  "https://cors-anywhere.herokuapp.com/http://api.weatherstack.com/current?access_key=e8278975abb258561367cab2590794a3&query= fetch:ip"
 )
   .then(res => res.json())
   .then(data => {
--- Weather/js/script.js
+++ Weather/js/script.js
@@ -1,7 +1,7 @@
 "use script";
 
 fetch(
-  "https://api.weatherstack.com/current?access_key=e8278975abb258561367cab2590794a3&query= fetch:ip"
+  "https://cors-anywhere.herokuapp.com/http://api.weatherstack.com/current?access_key=e8278975abb258561367cab2590794a3&query= fetch:ip"
 )
   .then(res => res.json())
   .then(data => {
