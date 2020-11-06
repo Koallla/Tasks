33008bf9d660527497dd7296b280977f1dd6bf5e
Mihail <41691809+Koallla@users.noreply.github.com>
2020-11-06 18:50:18 +0200

    Updates

---
+++ precache-manifest.3060485473dbc6789dd07e30bc8fcf1b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adfdf6c2761738648d7f7d0bff40dcae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2029d095c5a491f9e94a",
+    "url": "/static/js/2.bbccfd5f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.bbccfd5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "004c6b0e75a558ac3b8f",
+    "url": "/static/js/main.f9e0aead.chunk.js"
+  },
+  {
+    "revision": "d3529800ca9f2e3c0abd",
+    "url": "/static/js/runtime-main.cfef7d05.js"
+  }
+]);
